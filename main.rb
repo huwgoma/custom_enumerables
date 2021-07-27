80bde4697b0c45c0b24736570693042c9a404d2b
Hugo <huwgoma@gmail.com>
2021-07-27 00:09:03 -0400

    Hash enumerating

--- main.rb
+++ main.rb
@@ -2,7 +2,6 @@ require 'pry'
 
 module Enumerable
   def my_each
-    
     return Enumerator.new(self) unless block_given?
     
     enum_type = self.class
@@ -10,11 +9,10 @@ module Enumerable
     i = 0
     loop do 
       yield(self[i]) if enum_type == Array
-      
+      yield(self.keys[i], self.values[i]) if enum_type == Hash
       i+=1
       break if i > self.length - 1
     end
-    
   end
 end
 
@@ -26,12 +24,12 @@ puts "my_each vs. each"
 numbers = [1,2,3,4,5]
 hash = { a: 'a value', b: 'b value', c: 'c value' }
 
-numbers.each { |number| puts number }
-#hash.each { |key, value| puts "#{key}#{value}"}
+#numbers.each { |number| puts number }
+hash.each { |key, value| puts "#{key}: #{value}"}
 
 #
-#hash.my_each { |key, value| puts "#{key}#{value}"}
+hash.my_each { |key, value| puts "#{key}: #{value}"}
 
 #numbers.my_each { |number| puts number }
-binding.pry
-numbers.my_each
+
+#numbers.my_each
