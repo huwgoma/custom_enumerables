2143d15296c87d4210eccf8e6212e6493872568f
Hugo <huwgoma@gmail.com>
2021-07-26 23:58:34 -0400

    Initial commit - Return Enumerator if no block given; iterate through the Array/Hash and for each item, yield to the block - pass the current item through as a parameter

---
+++ main.rb
@@ -0,0 +1,37 @@
+require 'pry'
+
+module Enumerable
+  def my_each
+    
+    return Enumerator.new(self) unless block_given?
+    
+    enum_type = self.class
+    
+    i = 0
+    loop do 
+      yield(self[i]) if enum_type == Array
+      
+      i+=1
+      break if i > self.length - 1
+    end
+    
+  end
+end
+
+module Testable
+
+end
+
+puts "my_each vs. each"
+numbers = [1,2,3,4,5]
+hash = { a: 'a value', b: 'b value', c: 'c value' }
+
+numbers.each { |number| puts number }
+#hash.each { |key, value| puts "#{key}#{value}"}
+
+#
+#hash.my_each { |key, value| puts "#{key}#{value}"}
+
+#numbers.my_each { |number| puts number }
+binding.pry
+numbers.my_each
